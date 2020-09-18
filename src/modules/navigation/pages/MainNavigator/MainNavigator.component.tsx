f4030dd40aa06a8a4dfdcfff8ffb2796fc102976
Mithulan Vanniyasingam <cocmithulan@gmail.com>
2020-09-18 23:35:20 +0200

    feat: 🎸 MainNavigator

--- src/modules/navigation/pages/MainNavigator/MainNavigator.component.tsx
+++ src/modules/navigation/pages/MainNavigator/MainNavigator.component.tsx
@@ -1,7 +1,9 @@
 import React, { FunctionComponent } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Layout } from '../../../../components';
+import { EventPage } from '../../../Event/pages';
 import { HomePage } from '../../../home/pages';
+import { LifeStudentPage } from '../../../life_student/pages';
 import { PATHS } from '../../services';
 
 export const MainNavigator: FunctionComponent = () => {
@@ -11,6 +13,12 @@ export const MainNavigator: FunctionComponent = () => {
         <Switch>
           <Route exact path={PATHS.ROOT_PAGE.ROUTE}>
             <HomePage />
+          </Route>
+          <Route exact path="/evenement">
+            <EventPage />
+          </Route>
+          <Route exact path="/vie_etudiante">
+            <LifeStudentPage />
           </Route>
           <Route path="*">
             <div>404</div>

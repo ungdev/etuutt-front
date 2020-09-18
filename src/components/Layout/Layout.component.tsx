8e4951d2ef83ad78e074846bc938d9405e2bf9f9
Mithulan Vanniyasingam <cocmithulan@gmail.com>
2020-09-18 23:26:01 +0200

    feat: 🎸 Change of Layout files to add Navbar

--- src/components/Layout/Layout.component.tsx
+++ src/components/Layout/Layout.component.tsx
@@ -1,10 +1,13 @@
 import React, { FunctionComponent } from 'react';
+import { Navbar } from '../Navbar';
 import './Layout.style.scss';
 
 export const Layout: FunctionComponent = ({ children }) => {
   return (
     <div className="layout">
-      <div className="header">header</div>
+      <div className="header">
+        <Navbar></Navbar>
+      </div>
       <div className="content">{children}</div>
     </div>
   );

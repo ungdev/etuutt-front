b8c6d29f18a6657a12716bb3c0d49611f9e568aa
Mithulan Vanniyasingam <cocmithulan@gmail.com>
2020-09-18 23:34:12 +0200

    feat: 🎸 Navbar Files

--- src/components/Navbar/Navbar.component.tsx
+++ src/components/Navbar/Navbar.component.tsx
@@ -1,15 +1,50 @@
 import React, { FunctionComponent } from 'react';
+import city from './../../assets/city-hall.png';
+import home from './../../assets/home.png';
+import logo from './../../assets/logo.png';
+import timetable from './../../assets/timetable.png';
 import './Navbar.style.scss';
+
+const SideBarData = [
+  {
+    title: 'Accueil',
+    path: '/',
+    cName: 'nav-text',
+  },
+  {
+    title: 'Evenement',
+    path: '/evenement',
+    cName: 'nav-text',
+  },
+  {
+    title: 'Vie Etudiante',
+    path: 'vie_etudiante',
+    cName: 'nav-text',
+  },
+];
 
 export const Navbar: FunctionComponent = () => {
   return (
-    <div className="navbar">
-      <ul>
-        <a href="#Acceuil">Accueil</a>
-        <a href="#Evenement">Evenement</a>
-        <a href="#Forum">Forum</a>
-        <a href="#Vie etudiante">Vie Etudiante</a>
-      </ul>
-    </div>
+    <nav>
+      <img className="logo" src={logo} height="256" width="256"></img>
+      <li className="nav-text">
+        <a href="/">
+          <img src={home} height="64" width="64"></img>
+          <span>Accueil</span>
+        </a>
+      </li>
+      <li className="nav-text">
+        <a href="/evenement">
+          <img src={timetable} height="64" width="64"></img>
+          <span>Evenement</span>
+        </a>
+      </li>
+      <li className="nav-text">
+        <a href="/vie_etudiante">
+          <img src={city} height="64" width="64"></img>
+          <span>Vie Etudiante</span>
+        </a>
+      </li>
+    </nav>
   );
 };

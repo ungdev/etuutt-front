81d0341bf80797bdc8134283f13e8b13cb639cf9
Mithulan75 <57829726+Mithulan75@users.noreply.github.com>
2020-10-30 00:19:58 +0100

    :sparkles: (StyleGuidePage.component) examples of buttons

--- src/modules/Development/pages/StyleGuidePage/StyleGuidePage.component.tsx
+++ src/modules/Development/pages/StyleGuidePage/StyleGuidePage.component.tsx
@@ -1,5 +1,18 @@
 import React, { FunctionComponent } from 'react';
+import { Badge, Button } from '../../../../components';
 
 export const StyleGuidePage: FunctionComponent = () => {
-  return <div>StyleGuide Page</div>;
+  return (
+    <div>
+      <h1>StyleGuide Page</h1>
+      <h3>small button</h3>
+      <Button onClick={() => {}} title="Test" size="small" />
+      <h3>medium button</h3>
+      <Button onClick={() => {}} title="Test" size="medium" />
+      <h3>large button</h3>
+      <Button onClick={() => {}} title="Test" size="large" />
+      <h3>Bagde UE</h3>
+      <Badge onClick={() => {}} title="UE" />
+    </div>
+  );
 };

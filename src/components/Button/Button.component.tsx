8bc926baddb8eaa352097c18fad0cd9eb55db42e
Mithulan75 <57829726+Mithulan75@users.noreply.github.com>
2020-10-30 00:20:34 +0100

    :sparkles: (Button +Badge) components + style

--- src/components/Button/Button.component.tsx
+++ src/components/Button/Button.component.tsx
@@ -1,9 +1,30 @@
 import React, { FunctionComponent } from 'react';
+import './Button.style.scss';
+
+type ButtonSize = 'small' | 'medium' | 'large';
 
 interface ButtonProps {
   title: string;
+  size: ButtonSize;
+  onClick: () => void;
+}
+interface BadgeProps {
+  title: string;
+  onClick: () => void;
 }
 
-export const Button: FunctionComponent<ButtonProps> = ({ title }) => {
-  return <button className="button">{title}</button>;
+export const Button: FunctionComponent<ButtonProps> = ({ title, size, onClick }) => {
+  return (
+    <button onClick={onClick} className={`button button-${size}`}>
+      {title}
+    </button>
+  );
+};
+
+export const Badge: FunctionComponent<BadgeProps> = ({ title, onClick }) => {
+  return (
+    <button onClick={onClick} className={`badge button-large}`}>
+      {title}
+    </button>
+  );
 };

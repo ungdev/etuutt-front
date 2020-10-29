b5e3199358f79d303c5151f09491a7082a30b4eb
Mithulan75 <57829726+Mithulan75@users.noreply.github.com>
2020-10-29 20:49:25 +0100

    :sparkles: (MainNavigator.component) Ajout des routes pour le Navbar

--- src/modules/navigation/pages/MainNavigator/MainNavigator.component.tsx
+++ src/modules/navigation/pages/MainNavigator/MainNavigator.component.tsx
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Layout } from '../../../../components';
 import { EventPage } from '../../../Event/pages';
 import { HomePage } from '../../../home/pages';
+import { ServicePage } from '../../../services/ServicePage';
+import { TeamPage } from '../../../Team/TeamPage';
+import { GDPRPage } from '../../../GDPR/GDPRPage';
+import { BugPage } from '../../../Bug/BugPage';
+
 import { StudentLifePage } from '../../../studentLife/pages';
 import { PATHS } from '../../services';
 
@@ -19,6 +24,18 @@ export const MainNavigator: FunctionComponent = () => {
           </Route>
           <Route exact path={PATHS.STUDENT_LIFE_PAGE.ROUTE}>
             <StudentLifePage />
+          </Route>
+          <Route exact path={PATHS.SERVICES.ROUTE}>
+            <ServicePage />
+          </Route>
+          <Route exact path={PATHS.TEAM.ROUTE}>
+            <TeamPage />
+          </Route>
+          <Route exact path={PATHS.GDPR.ROUTE}>
+            <GDPRPage />
+          </Route>
+          <Route exact path={PATHS.BUG.ROUTE}>
+            <BugPage />
           </Route>
           <Route path="*">
             <div>404</div>

00c07763ba747c889c7cb73f660975c39529d0f3
Rinkichi75 <cocmithulan@gmail.com>
2022-04-29 10:30:21 +0200

    Feat:(Card) implement in StudentLifePage for an example

--- src/modules/studentLife/pages/StudentLifePage/StudentLifePage.component.tsx
+++ src/modules/studentLife/pages/StudentLifePage/StudentLifePage.component.tsx
@@ -1,5 +1,19 @@
 import React, { FunctionComponent } from 'react';
-
+import { Card } from '../../../../components';
+import logo from './../../../../assets/logo.png';
 export const StudentLifePage: FunctionComponent = () => {
-  return <div>Vie Etudiante yolo</div>;
+  return (
+    <div className="card-container">
+      <div className="card-grid">
+        <Card
+          title={'Game UTT'}
+          description={`Club de jeux video d'école`}
+          logo={logo}
+          onClick={function (): void {
+            throw new Error('Function not implemented.');
+          }}
+        />
+      </div>
+    </div>
+  );
 };

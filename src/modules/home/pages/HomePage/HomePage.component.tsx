82c262f75a0da9a2a15ab7fb0750aedbde07c607
Rinkichi75 <cocmithulan@gmail.com>
2022-05-10 13:23:20 +0200

    test/ API: test on route /users still has the problem with CORS

--- src/modules/home/pages/HomePage/HomePage.component.tsx
+++ src/modules/home/pages/HomePage/HomePage.component.tsx
@@ -1,5 +1,17 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import { API } from '../../../../utils/api';
 
 export const HomePage: FunctionComponent = () => {
+  const [user, setUser] = useState();
+  const fetchUsers = async () => {
+    const res = await API.get(`/users`, 'admin');
+  };
+  // Fetch the tournaments once, after the first render
+  useEffect(() => {
+    if (!user) {
+      fetchUsers();
+    }
+  }, []);
+
   return <div>home</div>;
 };

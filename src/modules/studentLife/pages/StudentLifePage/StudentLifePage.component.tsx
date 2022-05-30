4c0fae30c573f10f891ce1dbb423d2c892abb7e0
Rinkichi75 <cocmithulan@gmail.com>
2022-05-30 18:16:14 +0200

    FEAT/StudentLifePage: test with several card + style

--- src/modules/studentLife/pages/StudentLifePage/StudentLifePage.component.tsx
+++ src/modules/studentLife/pages/StudentLifePage/StudentLifePage.component.tsx
@@ -1,13 +1,39 @@
 import React, { FunctionComponent } from 'react';
 import { Card } from '../../../../components';
 import logo from './../../../../assets/logo.png';
+import './StudentLifePage.style.scss';
+
 export const StudentLifePage: FunctionComponent = () => {
   return (
     <div className="card-container">
       <div className="card-grid">
         <Card
           title={'Game UTT'}
-          description={`Club de jeux video d'école`}
+          description={`Lorem ipsum dolor sit amet. A reprehenderit nemo aut voluptatem voluptatem ut modi illo At vitae quam ut accusantium odio. `}
+          logo={logo}
+          onClick={function (): void {
+            throw new Error('Function not implemented.');
+          }}
+        />
+        <Card
+          title={'Game UTT'}
+          description={`Lorem ipsum dolor sit amet. A reprehenderit nemo aut voluptatem voluptatem ut modi illo At vitae quam ut accusantium odio. `}
+          logo={logo}
+          onClick={function (): void {
+            throw new Error('Function not implemented.');
+          }}
+        />
+        <Card
+          title={'Game UTT'}
+          description={`Lorem ipsum dolor sit amet. A reprehenderit nemo aut voluptatem voluptatem ut modi illo At vitae quam ut accusantium odio. `}
+          logo={logo}
+          onClick={function (): void {
+            throw new Error('Function not implemented.');
+          }}
+        />
+        <Card
+          title={'Game UTT'}
+          description={`Lorem ipsum dolor sit amet. A reprehenderit nemo aut voluptatem voluptatem ut modi illo At vitae quam ut accusantium odio. `}
           logo={logo}
           onClick={function (): void {
             throw new Error('Function not implemented.');
